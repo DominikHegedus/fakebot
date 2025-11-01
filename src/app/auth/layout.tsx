6f0d6233bfac2725231b0a7f017b862ef566e0d8
Dominik Hegedus <hege0123@gmail.com>
2025-11-01 20:12:20 +0100

    style login

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,32 +1,27 @@
+import { Card, CardContent } from "@/components/ui/card";
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="grid min-h-svh lg:grid-cols-2">
-      <div className="flex flex-col gap-4 p-6 md:p-10">
-        <div className="flex justify-center gap-2 md:justify-start">
-          <a
-            href="#"
-            className="flex items-center gap-2 font-medium"
-          >
-            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-              Logo
-            </div>
-            Acme Inc.
-          </a>
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-4xl">
+        <div className="flex flex-col gap-6">
+          <Card className="overflow-hidden p-0">
+            <CardContent className="grid p-0 md:grid-cols-2">
+              <div className="p-6 md:p-8">{children}</div>
+              <div className="bg-muted relative hidden md:block">
+                <img
+                  src="/placeholder.svg"
+                  alt="Image"
+                  className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+                />
+              </div>
+            </CardContent>
+          </Card>
         </div>
-        <div className="flex flex-1 items-center justify-center">
-          <div className="w-full max-w-xs">{children}</div>
-        </div>
-      </div>
-      <div className="bg-muted relative hidden lg:block">
-        <img
-          src="/placeholder.svg"
-          alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
       </div>
     </div>
   );
