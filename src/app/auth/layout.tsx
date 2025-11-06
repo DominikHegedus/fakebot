9a9f3a64b78a12262dde3c585252e8a782dca325
Dominik Hegedus <hege0123@gmail.com>
2025-11-06 17:00:40 +0100

    add extra bg

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,3 +1,4 @@
+import { BackgroundGradientAnimation } from "@/components/ui/background-gradient-animation";
 import { Card, CardContent } from "@/components/ui/card";
 import Image from "next/image";
 
@@ -7,32 +8,34 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-4xl">
-        <div className="flex flex-col gap-6">
-          <div className="w-full flex justify-center">
-            <Image
-              className="rounded-md"
-              src="/fakebot-wo-text.png"
-              alt="Logo"
-              width={64}
-              height={64}
-            />
+    <BackgroundGradientAnimation>
+      <div className="absolute z-50 inset-0 flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+        <div className="w-full max-w-sm md:max-w-4xl">
+          <div className="flex flex-col gap-6">
+            <div className="w-full flex justify-center">
+              <Image
+                className="rounded-md"
+                src="/fakebot-wo-text.png"
+                alt="Logo"
+                width={64}
+                height={64}
+              />
+            </div>
+            <Card className="overflow-hidden p-0">
+              <CardContent className="grid p-0 md:grid-cols-2">
+                <div className="p-6 md:p-8">{children}</div>
+                <div className="bg-muted relative hidden md:block">
+                  <img
+                    src="/fakebot-messaging.png"
+                    alt="example of a messaging app"
+                    className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+                  />
+                </div>
+              </CardContent>
+            </Card>
           </div>
-          <Card className="overflow-hidden p-0">
-            <CardContent className="grid p-0 md:grid-cols-2">
-              <div className="p-6 md:p-8">{children}</div>
-              <div className="bg-muted relative hidden md:block">
-                <img
-                  src="/fakebot-messaging.png"
-                  alt="example of a messaging app"
-                  className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-                />
-              </div>
-            </CardContent>
-          </Card>
         </div>
       </div>
-    </div>
+    </BackgroundGradientAnimation>
   );
 }
